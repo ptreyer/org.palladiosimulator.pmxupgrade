6a01a81bf6ca7762df3e87e29c445dda6fc26879
ptreyer <MVRitDW2BxsA91ZFE18G>
2020-07-20 09:30:42 +0200

    preparation of more filters

---
+++ src/main/java/de/kit/research/logic/filter/opentracing/CPUFilter.java
@@ -0,0 +1,13 @@
+package de.kit.research.logic.filter.opentracing;
+
+import de.kit.research.logic.filter.AbstractTraceProcessingFilter;
+import de.kit.research.model.common.Configuration;
+import de.kit.research.model.inputreader.opentracing.jaeger.TraceRecord;
+
+public class CPUFilter implements AbstractTraceProcessingFilter {
+
+    @Override
+    public TraceRecord filter(Configuration configuration, TraceRecord traceRecord) {
+        return null;
+    }
+}
