bae0ecf18fdc4eebfce90e957aa970814749261b
ptreyer <MVRitDW2BxsA91ZFE18G>
2020-07-23 14:24:07 +0200

    adds tests for data preparation

---
+++ src/test/java/de/kit/research/filter/TraceReconstructionFilterTest.java
@@ -0,0 +1,32 @@
+package de.kit.research.filter;
+
+import de.kit.research.logic.PMXController;
+import de.kit.research.logic.filter.opentracing.TraceReconstructionFilter;
+import de.kit.research.model.common.Configuration;
+import de.kit.research.model.exception.PMXException;
+import de.kit.research.model.inputreader.opentracing.jaeger.Span;
+import de.kit.research.model.inputreader.opentracing.jaeger.Trace;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
+public class TraceReconstructionFilterTest {
+
+    @Test
+    void filter() throws PMXException, FileNotFoundException, UnsupportedEncodingException {
+        Configuration configuration = new Configuration();
+        configuration.setInputFileName("C:\\Users\\ptreyer\\workspace\\pmxupgrade\\src\\test\\resources\\json\\combination3.json");
+
+        PMXController pmxController = new PMXController(configuration);
+        pmxController.readTracingData();
+
+        TraceReconstructionFilter filter = new TraceReconstructionFilter();
+
+        filter.filter(configuration, pmxController.getTraceRecord());
+
+        filter.systemModelRepository.saveSystemToHTMLFile("test");
+
+        System.out.println("finish");
+    }
+}
