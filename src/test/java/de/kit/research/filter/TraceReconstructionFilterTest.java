2c922a1ea47218485484c8885259e5afaa396a50
ptreyer <MVRitDW2BxsA91ZFE18G>
2020-07-27 09:36:31 +0200

    prepares WorkloadService

--- src/test/java/de/kit/research/filter/TraceReconstructionFilterTest.java
+++ src/test/java/de/kit/research/filter/TraceReconstructionFilterTest.java
@@ -16,7 +16,7 @@ public class TraceReconstructionFilterTest {
     @Test
     void filter() throws PMXException, FileNotFoundException, UnsupportedEncodingException {
         Configuration configuration = new Configuration();
-        configuration.setInputFileName("C:\\Users\\ptreyer\\workspace\\pmxupgrade\\src\\test\\resources\\json\\trace1.json");
+        configuration.setInputFileName("C:\\Users\\ptreyer\\workspace\\pmxupgrade\\src\\test\\resources\\json\\combination3.json");
 
         PMXController pmxController = new PMXController(configuration);
         pmxController.readTracingData();
@@ -25,7 +25,7 @@ public class TraceReconstructionFilterTest {
 
         filter.filter(configuration, pmxController.getTraceRecord());
 
-        filter.systemModelRepository.saveSystemToHTMLFile("test");
+        filter.systemModelRepository.saveSystemToHTMLFile("test.html");
 
         System.out.println("finish");
     }
